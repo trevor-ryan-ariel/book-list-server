f3777ff7d73d158e1e0b587b0123bd2d7a2714cb
stariel <ariel.altaras@gmail.com>
2018-01-08 19:17:36 -0800

    edits to server.js to complete route

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 
 const express = require('express');
 const pg = require('pg');
-const bodyParser = require('body-parser');
-const fs = require('fs');
 const cors = require('cors');
 
 
@@ -11,23 +9,24 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 //consString for Trevor
-// const conString = 'postgres://trevorjdobson:1234@localhost:5432/books_app';
+// const DATABASE_URL = process.env.DATABASE_URL || 'postgres://trevorjdobson:1234@localhost:5432/books_app';
 //conString for Ryan
-const conString = 'postgres://localhost:5432/books_app';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app';
 
-const client = new pg.Client(conString);
+const client = new pg.Client(DATABASE_URL);
 client.connect();
 client.on('error', err => {
-    console.error(err);
+  console.error(err);
 });
 
 app.get('/test', (req, res) => res.send('hello world'));
 
 app.get('/api/v1/books', (req, res) => {
-    client.query(`
+  client.query(`
         SELECT book_id, title, author, image_url FROM books;
         `)
-    .then(result => consoel.log(result)
+    .then(result => res.send(result.rows))
+    .catch(err => console.error(err));
 });
 
 app.listen(PORT, () => console.log('server started on port' + PORT));
